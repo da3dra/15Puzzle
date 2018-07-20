9968acd0f57e2acb996fa9021143f7040c71af21
Daria <da3draedr@gmail.com>
2018-07-20 15:05:15 +0300

    opened Puzzle methods for testing and refactored tests

--- test/PuzzleTests.java
+++ test/PuzzleTests.java
@@ -1,50 +1,35 @@
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-
-// I used Reflection to avoid making private fields and randomize() method visible
+import static org.junit.Assert.*;
 
 public class PuzzleTests {
 
-    Puzzle mockPuzzle;
+    private Puzzle mockPuzzle;
 
     // tests if array is shuffled after randomize() method has been called
     @Test
-    public void testRandomization() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void testRandomization() {
         mockPuzzle = new Puzzle(4);
-        Method randomizeMethod = Puzzle.class.getDeclaredMethod("randomize");
-        randomizeMethod.setAccessible(true);
         int[][] puzzleArr = mockPuzzle.getPuzzleMatrix();
         int size = mockPuzzle.getSize();
         int[][] initialArray = new int[size][size];
         for(int i=0; i<size; i++)
-            for(int j=0; j<size; j++)
-                initialArray[i][j]=puzzleArr[i][j];
-        randomizeMethod.invoke(mockPuzzle);
+            System.arraycopy(puzzleArr[i], 0, initialArray[i], 0, size);
+        mockPuzzle.randomize();
         assertThat(initialArray, not(equalTo(mockPuzzle.getPuzzleMatrix())));
     }
 
     // tests if isSolved flag is true after performing the last move in correct direction
     @Test
-    public void testIfSolved() throws NoSuchFieldException, IllegalAccessException {
+    public void testIfSolved()  {
         mockPuzzle = new Puzzle(4);
         int [][] testArr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,0,15}};
-        Field zeroCol = Puzzle.class.getDeclaredField("zeroColumn");
-        zeroCol.setAccessible(true);
-        zeroCol.set(mockPuzzle, 2);
-        Field zeroRow = Puzzle.class.getDeclaredField("zeroRow");
-        zeroRow.setAccessible(true);
-        zeroRow.set(mockPuzzle, 3);
-        Field puzzleMatrix = Puzzle.class.getDeclaredField("puzzleMatrix");
-        puzzleMatrix.setAccessible(true);
-        puzzleMatrix.set(mockPuzzle, testArr);
+        mockPuzzle.setZeroColumn(2);
+        mockPuzzle.setZeroRow(3);
+        mockPuzzle.setPuzzleMatrix(testArr);
         mockPuzzle.moveRight();
-        assertEquals(true,mockPuzzle.isSolved());
+        assertTrue(mockPuzzle.isSolved());
     }
 }
